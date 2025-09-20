29286b227fab770ae60331092987e4abcde7786e
NamanPro24 <namanprogrammer24@gmail.com>
2025-09-20 14:28:05 +0530

    I am working on  react 2nd demo project

---
+++ Demo2/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>
+    <h1 style={{textAlign:"center"}}>Welcome To React Demo Project 2nd</h1>
+   </div>
+   
+  )
+}
+
+export default App
